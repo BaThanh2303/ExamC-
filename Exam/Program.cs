3be99c1bc245acf02c8057cd9824bc879fe8d531
BaThanh2303 <thanhnbth2210021@fpt.edu.vn>
2023-08-30 15:23:54 +0700

    Add project files.

---
+++ Exam/Program.cs
@@ -0,0 +1,20 @@
+﻿using Exam;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Lion lion = new Lion(200, "lion");
+        Tiger tiger = new Tiger(100, "tiger");
+
+        lion.Show();
+        tiger.Show();
+    }
+    public static void Main1(string[] args)
+    {
+        Cylinder cylinder = new Cylinder();
+        cylinder.Process();
+        cylinder.Result();
+
+    }
+}
